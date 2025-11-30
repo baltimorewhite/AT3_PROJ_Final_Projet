39a54f64555a0c3a86fbdb0337a4b746f65fa9f1
baltimoreWhite <baltimorewhite@gmail.com>
2025-11-30 22:34:55 +0800

    feat(nav): add toggle functionality with JavaScript

--- tailwind.config.js
+++ tailwind.config.js
@@ -1,18 +1,21 @@
 /** @type {import('tailwindcss').Config} */
 module.exports = {
-  content: ["./*.html"],
+  darkMode: 'class',
+  content: ['./**/*.html'],
   theme: {
     extend: {
       screens:{
-        "sm":"480px"
+        'sm':'480px',
+        'md':"768px",
+        'lg':"1024px",
+        'xl':"1280px",
+
       }
     },
     fontFamily:{
       nunito:['Nunito', 'sans-serif']
     },
-    colors:{
-      
-    }
+
   },
   plugins: [],
 }
