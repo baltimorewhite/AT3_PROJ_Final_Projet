4b902f2651456bdeedcbc88d22fa38a5cf5671dd
baltimoreWhite <baltimorewhite@gmail.com>
2025-12-01 01:37:01 +0800

    feat(home): add profile photo with centered background for large screens

--- tailwind.config.js
+++ tailwind.config.js
@@ -1,22 +1,24 @@
 /** @type {import('tailwindcss').Config} */
 module.exports = {
-  darkMode: 'class',
-  content: ['./**/*.html'],
+  darkMode:"class",
+  content: ['./*.html'],
   theme: {
     extend: {
-      screens:{
-        'sm':'480px',
-        'md':"768px",
-        'lg':"1024px",
-        'xl':"1280px",
-
+      spacing:{
+        "big":"48rem"
       }
+    },
+    screens:{
+      sm:"480px",
+      md:"768px",
+      lg:"1024px",
+      xl:"1280px",
     },
     fontFamily:{
       nunito:['Nunito', 'sans-serif']
-    },
-
+    }
   },
   plugins: [],
 }
+
 
